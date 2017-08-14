d52a05b6226fc2f7880754f37e38d992889191ac
皮皮虾 <pipixia@pipi.com>
2017-08-14 17:13:28 +0800

    将之前的内容改造成模块化

--- views/public/js/common.js
+++ views/public/js/common.js
@@ -7,16 +7,18 @@
 	// 	$(this).next().slideToggle();
 	// });
 
-	// 验证是否登陆，如果没有登陆的话，则跳转到登陆页面
-	if(!$.cookie('PHPSESSID')&&location.pathname!='/login'){
-	///views/dashboard/login
-		 window.location.href= '/login';
-	}
+	define(['jquery','cookie','template'],function ($,ck,template){
+    // 验证是否登陆，如果没有登陆的话，则跳转到登陆页面
+    if(!$.cookie('PHPSESSID')&&location.pathname!='/login'){
+      ///views/dashboard/login
+      window.location.href= '/login';
+    }
 
-	// 判断  因为login页面是没有侧边栏和顶部的  因此需要排队掉首页面
+    // 判断  因为login页面是没有侧边栏和顶部的  因此需要排队掉首页面
 
- 	if(location.pathname!='/login'&&location.pathname!='/dashboard/login'&&location.pathname!='/views/dashboard/login'){
-    var html = template('tpl_profile',JSON.parse($.cookie('tcInfo')));
-    $('.aside>.profile').html(html);
-	}
+    if(location.pathname!='/login'&&location.pathname!='/dashboard/login'&&location.pathname!='/views/dashboard/login'){
+      var html = template('tpl_profile',JSON.parse($.cookie('tcInfo')));
+      $('.aside>.profile').html(html);
+    }
+	})
 

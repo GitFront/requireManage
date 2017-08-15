c7f542b2740257c467c61f99b9d3fe0c46ba285b
皮皮虾 <pipixia@pipi.com>
2017-08-15 09:27:17 +0800

    提取模块的配置文件到单独的文件夹中

--- views/public/libs/config.js
+++ views/public/libs/config.js
@@ -1,3 +1,22 @@
 /**
  * Created by Administrator on 2017/8/15.
  */
+require.config({
+  baseUrl: '/views/public',
+  paths: {
+    'jquery': 'assets/jquery/jquery',
+    'cookie': 'assets/jquery-cookie/jquery.cookie',
+    'bootstrap': 'assets/bootstrap/js/bootstrap.min',
+    'template': 'assets/artTemplate/template',
+    'nprogress': 'assets/nprogress/nprogress',
+//      'common': 'js/common'
+    'common':'js/dashboard/common',
+    'login':'js/dashboard/login'
+  },
+  shim: {
+    bootstrap:{
+      deps:['jquery']
+    }
+  }
+});
+require(['common']);
